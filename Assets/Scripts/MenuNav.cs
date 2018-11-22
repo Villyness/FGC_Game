18664390c2ea57f8597c96aa34ebebe8b5387e04
Tegan Walsh-Jones <1009559@student.sae.edu.au>
2018-11-22 20:33:03 +1100

    Added Main Menu (including code); Moved scripts in to general Assets folder

---
+++ Assets/Scripts/MenuNav.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuNav : MonoBehaviour
+{
+	[SerializeField] private Button startGame;
+	[SerializeField] private Button credits;
+	[SerializeField] private Button exitApp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Button sbtn = startGame.GetComponent<Button>();
+		sbtn.onClick.AddListener(StartGame);
+
+		Button cbtn = credits.GetComponent<Button>();
+		cbtn.onClick.AddListener(Credits);
+
+		Button ebtn = exitApp.GetComponent<Button>();
+		ebtn.onClick.AddListener(ExitApp);
+	}
+
+	void StartGame ()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	void Credits ()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	void ExitApp ()
+	{
+		Application.Quit();
+	}
+}
